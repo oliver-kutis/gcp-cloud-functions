b38d75b596ca10cdd23cd1c7239d096e834b0f02
Oliver131313 <kutisoliver@outlook.com>
2024-08-29 21:46:44 +0200

    Added two existing cloud functions.

---
+++ niftyminds-client-reporting/gen2_http_client_reporting_dataform_trigger/main.py
@@ -0,0 +1,305 @@
+import functions_framework
+import json
+import time
+from google.cloud import logging
+from google.cloud import dataform_v1beta1 as dataform
+# from google.cloud.dataform_v1beta1.types import WorkflowInvocation
+
+DEFAULTS_ARG_VALUES = {
+    'project_id': 'niftyminds-client-reporting',
+    'location': 'europe-west3',
+    'git_commitish': "main",
+}
+GLOBAL_LOG_FIELDS = {
+    'pipeline_phase': 'Dataform workflow exection'
+}
+FUNCTION_ARGS = {
+    "required": ['dataform_repo_name', 'client_name'],
+    "optional": ['git_commitish',
+                 'project_id', 'location']
+}
+inputs_dict = dict()
+
+
+@functions_framework.http
+def run(request):
+    """
+    This function is an HTTP Cloud Function that expects an HTTP POST request with
+    a JSON body containing the following parameters:
+        - keboola_endpoint_url: The URL of the Keboola Orchestrator API
+        - keboola_storage_api_token: The Storage API token to use for authentication
+        - keboola_orchestration_trigger_run_id: The ID of the orchestration trigger run
+        - keboola_job_max_runtime_seconds: The maximum amount of time the job should run
+
+    If the job finishes successfully, the function returns "Good". If the job fails
+    or times out, the function returns "Bad".
+    """
+
+    gcp_log(
+        "NOTICE",
+        f"Function started. Parameters: {request.get_json(silent=True)}",
+    )
+
+    # check request arguments
+    check_request_args_result = check_request_args(request)
+    if (check_request_args_result[1] == 400):
+        return check_request_args_result
+    else:
+        inputs_dict = check_request_args_result[0]
+
+    # compile dataform code
+    compilation_result = create_dataform_compilation(inputs_dict)
+    # If errror in create_job, return the error message
+    if compilation_result[1] == 400:
+        return compilation_result
+    else:
+        compilation_name = compilation_result[0]
+
+    # invoke dataform workflow
+    invocation_result = create_workflow_invocation(
+        compilation_name, inputs_dict)
+    if invocation_result[1] == 400:
+        return invocation_result[0]
+    else:
+        invocation_name = invocation_result[0]
+
+    # check the result of the incovation
+    execution_result = check_workflow_execution_result(
+        invocation_name, inputs_dict)
+    if execution_result[1] == 400:
+        return execution_result[0]
+    else:
+        message = f"Function finished for client: '{inputs_dict['client_name']}' and repostiory: '{inputs_dict['dataform_repo_name']}'"
+        gcp_log(
+            "NOTICE",
+            message,
+            dict(job_phase="result", status_code=execution_result[1], status=str(
+                execution_result[0]))
+        )
+
+        return (message, execution_result[1])
+
+
+def create_dataform_compilation(inputs_dict):
+    job_phase = "create_dataform_compilation"
+    # Compilation result
+    try:
+        parent = f"projects/{inputs_dict['project_id']}/locations/{inputs_dict['location']}/repositories/{inputs_dict['dataform_repo_name']}"
+        client = dataform.DataformClient()
+
+        compilation_result = dataform.CompilationResult()
+        compilation_result.git_commitish = inputs_dict["git_commitish"]
+
+        request = dataform.CreateCompilationResultRequest(
+            parent=parent, compilation_result=compilation_result)
+
+        result = client.create_compilation_result(request)
+
+        gcp_log(
+            "INFO",
+            f"Compilation result created: {compilation_result}",
+            dict(job_phase=job_phase, job_phase_detail="compilation_result",
+                 compilation_result=str(result))
+        )
+
+        return (result.name, 200)
+
+    except Exception as e:
+        return gcp_log(
+            "ERROR",
+            f"Failed to create dataform compilation result for repository: {inputs_dict['dataform_repo_name']}.",
+            dict(job_phase=job_phase, job_phase_detail="compilation_result",
+                 error_message=str(e))
+        )
+
+    return (str(workflow_invocation_request_result), 200)
+
+
+def create_workflow_invocation(compilation_name, inputs_dict):
+    job_phase = "create_workflow_invocation"
+    try:
+        parent = f"projects/{inputs_dict['project_id']}/locations/{inputs_dict['location']}/repositories/{inputs_dict['dataform_repo_name']}"
+        client = dataform.DataformClient()
+
+        workflow_invocation = dataform.WorkflowInvocation()
+        workflow_invocation.compilation_result = compilation_name
+
+        if 'tags' in inputs_dict:
+            workflow_invocation.invocation_config = {
+                "included_tags": inputs_dict['tags']
+            }
+
+        request = dataform.CreateWorkflowInvocationRequest(
+            parent=parent,
+            workflow_invocation=workflow_invocation
+        )
+
+        result = client.create_workflow_invocation(request)
+
+        gcp_log(
+            "INFO",
+            f"Workflow invocation created: {result}",
+            dict(job_phase=job_phase, job_phase_detail="workflow_invocation",
+                 workflow_invocation_result=str(result))
+        )
+
+        return (result.name, 200)
+    except Exception as e:
+        return gcp_log(
+            "ERROR",
+            f"Failed to create dataform workflow invocation for repository: {inputs_dict['dataform_repo_name']}.",
+            dict(job_phase=job_phase, job_phase_detail="workflow_invocation",
+                 error_message=str(e)
+                 )
+        )
+
+
+def check_workflow_execution_result(invocation_name, inputs_dict):
+    job_phase = "check_workflow_execution_result"
+    try:
+        invocation_id = invocation_name.split("/")[-1]
+        client = dataform.DataformClient()
+        request = dataform.GetWorkflowInvocationRequest(name=invocation_name)
+
+        execution_completed = False
+
+        while execution_completed != True:
+            result = client.get_workflow_invocation(request)
+            state = dataform.WorkflowInvocation.State(result.state).name
+            if state == "RUNNING" or state == "CANCELING":
+                gcp_log(
+                    "INFO",
+                    f"Dataform execution is in state {state}, falling to sleep for 10 seconds",
+                    dict(job_phase=job_phase,
+                         job_phase_detail="running_or_canceling")
+                )
+
+                time.sleep(10)
+
+            else:
+                execution_completed = True
+                execution_time = result.invocation_timing.end_time.seconds - \
+                    result.invocation_timing.start_time.seconds
+                message = f"The dataform execution {state} in {execution_time} seconds for client: '{inputs_dict['client_name']}' in repository: '{inputs_dict['dataform_repo_name']}'",
+                add_args_dict = dict(
+                    job_phase=job_phase,
+                    invocation_id=invocation_id
+                )
+                if state == "SUCCEEDED":
+                    gcp_log("NOTICE", message, add_args_dict)
+                elif state == "CANCELLED":
+                    gcp_log("WARNING", message, add_args_dict)
+                elif state == "FAILED":
+                    return gcp_log("ERROR", message, add_args_dict)
+                break
+
+        return (result, 200)
+    except Exception as e:
+        return gcp_log(
+            "ERROR",
+            f"Failed to check the dataform execution due to an error: {e}",
+            dict(
+                job_phase=job_phase,
+                error_message=str(e)
+            )
+        )
+
+
+def check_request_args(request):
+    request_json = request.get_json(silent=False)
+    inputs_dict = dict()
+    job_phase = "check_request_args"
+    # Log that the check of args started
+    gcp_log(
+        "INFO",
+        "Checking the request args...",
+        dict(job_phase="check_request_args")
+    )
+    if not request_json:
+        return gcp_log(
+            "ERROR",
+            "The body of the request is missing. Please enter a body with valid values for: {FUNCTION_ARGS}.",
+            dict(job_phase=job_phase,
+                 job_phase_detail="request_body")
+        )
+
+    # Check optional args
+    for arg in FUNCTION_ARGS['optional']:
+        if arg in request_json and request_json[arg] is not None:
+            inputs_dict[arg] = request_json[arg]
+        else:
+            inputs_dict[arg] = DEFAULTS_ARG_VALUES[arg]
+            gcp_log(
+                "WARNING",
+                f"The '{arg}' wasn't provided or the value is None. Using default value = '{DEFAULTS_ARG_VALUES[arg]}' instead.",
+                dict(job_phase=job_phase,
+                     job_phase_detail="optional_args",
+                     arg_details={
+                         "name": arg,
+                         "value": DEFAULTS_ARG_VALUES[arg]
+                     })
+            )
+
+    # Check required args
+    for arg in FUNCTION_ARGS['required']:
+        if arg in request_json and request_json[arg] is not None:
+            inputs_dict[arg] = request_json[arg]
+            if arg == 'client_name':
+                GLOBAL_LOG_FIELDS['client_name'] = request_json['client_name']
+        else:
+            return gcp_log(
+                "ERROR",
+                f"The '{arg}' wasn't provided or the value is None. Please enter a value for '{arg}'.",
+                dict(job_phase=job_phase,
+                     job_phase_detail="required_args",
+                     arg_details={
+                         "name": arg,
+                         "value": request_json.get(arg),
+
+                     })
+            )
+
+    GLOBAL_LOG_FIELDS['input_params'] = inputs_dict
+    gcp_log(
+        "NOTICE",
+        f"All request arguments checked. Parameters: {inputs_dict}",
+        {"parameters_all": inputs_dict}
+    )
+
+    # print("-----inputs_dict", inputs_dict)
+    return (inputs_dict, 200)
+
+
+def gcp_log(severity, message, additional_log_fields=None):
+    """
+    Logs a message into Google Cloud Functions logs.
+
+    Args:
+        severity (str): The severity of the log message. Supported values are:
+            "DEBUG", "INFO", "NOTICE", "WARNING", "ERROR", "CRITICAL", "ALERT",
+            "EMERGENCY".
+        message (str): The message to be logged.
+        **kwargs: Additional keyword arguments to be added to the log entry.
+    """
+    if additional_log_fields is None:
+        additional_log_fields = {}
+
+    # Add client_name to additional_log_fields if it is present in inputs_dict
+    # if 'client_name' in inputs_dict:
+    #     additional_log_fields['client_name'] = inputs_dict['client_name']
+
+    additional_log_fields = {**GLOBAL_LOG_FIELDS, **additional_log_fields}
+
+    log_entry = dict(
+        severity=severity.upper(),
+        message=message,
+        component="Dataform trigger",
+        **additional_log_fields
+    )
+
+    print(json.dumps(log_entry))
+
+    if severity.upper() == "ERROR":
+        return ({"error": message, "details": additional_log_fields, }, 400)
+
+    return ({"message": message, "details": additional_log_fields}, 200)
---
+++ niftyminds-client-reporting/gen2_http_client_reporting_keboola_orchestrationV2_trigger/main.py
@@ -0,0 +1,401 @@
+import functions_framework
+import time
+import requests
+import json
+
+KEBOOLA_COMPONENT_NAME = "kds-team.app-orchestration-trigger-queue-v2"
+KEBOOLA_MODE = "run"
+DEFAULTS_ARG_VALUES = {
+    'keboola_endpoint_url': 'https://queue.north-europe.azure.keboola.com',
+    'keboola_storage_api_token': '21333-3956626-gIqAxVgA9cFit7cnJ5aJrNI5iOFLgJUksFDQgRJ6',
+    'keboola_job_max_runtime_seconds': 360,
+}
+GLOBAL_LOG_FIELDS = {
+    'pipeline_phase': 'Keboola orchestration trigger'
+}
+FUNCTION_ARGS = {
+    "required": ['keboola_orchestration_trigger_run_id', 'client_name'],
+    "optional": ['keboola_storage_api_token',
+                 'keboola_job_max_runtime_seconds', 'keboola_endpoint_url']
+}
+
+inputs_dict = dict()
+
+
+@functions_framework.http
+def run(request):
+    """
+    This function is an HTTP Cloud Function that expects an HTTP POST request with
+    a JSON body containing the following parameters:
+        - keboola_endpoint_url: The URL of the Keboola Orchestrator API
+        - keboola_storage_api_token: The Storage API token to use for authentication
+        - keboola_orchestration_trigger_run_id: The ID of the orchestration trigger run
+        - keboola_job_max_runtime_seconds: The maximum amount of time the job should run
+
+    If the job finishes successfully, the function returns "Good". If the job fails
+    or times out, the function returns "Bad".
+    """
+
+    gcp_log(
+        "NOTICE",
+        f"Function started. Parameters: {request.get_json(silent=True)}",
+    )
+    check_request_args_result = check_request_args(request)
+    # If error in check_request_args, return the error message
+    if (check_request_args_result[1] == 400):
+        return check_request_args_result
+    else:
+        inputs_dict = check_request_args_result[0]
+
+    create_job_result = create_job(inputs_dict['keboola_endpoint_url'], inputs_dict['keboola_storage_api_token'],
+                                   inputs_dict['keboola_orchestration_trigger_run_id'])
+    # If errror in create_job, return the error message
+    if create_job_result[1] == 400:
+        return create_job_result
+    else:
+        job_id = create_job_result[0]
+
+    check_job_status_result = check_job_status(inputs_dict['keboola_endpoint_url'], inputs_dict['keboola_storage_api_token'], job_id,
+                                               inputs_dict['keboola_job_max_runtime_seconds'])
+    if check_job_status_result[1] == 400:
+        return check_job_status_result
+    else:
+        message = f"Function finished. {check_job_status_result[0]} for '{inputs_dict['client_name']}'.",
+        gcp_log(
+            "NOTICE",
+            message,
+            dict(job_phase="result", status_code=check_job_status_result[1]),
+        )
+        return (message, check_job_status_result[1])
+
+    # # If error in check_job_status, return the error message
+    # # if results[1] == 400:
+    # #     return results
+    # # else:
+
+    # results_action = results[0]
+    # results_status = results[1]
+    # if results_action == 'Killed' or results_status == 'failed':
+    #     severity = "ERROR"
+    #     status_code = 400
+    # else:
+    #     severity = "NOTICE"
+    #     status_code = 200
+
+    # message = f"Function {results_action.upper()} job ({job_id}) for '{inputs_dict['client_name']}' with latest job status: {results_status}"
+    # gcp_log(
+    #     severity,
+    #     f"Function {results_action.upper()} job ({job_id}) for '{inputs_dict['client_name']}' with latest job status: {results_status}",
+    #     dict(job_phase="result", status_code=status_code),
+    #     True
+    # )
+
+    # return (message, status_code)
+
+
+def check_request_args(request):
+    request_json = request.get_json(silent=False)
+    inputs_dict = dict()
+    job_phase = "check_request_args"
+    # Log that the check of args started
+    gcp_log(
+        "INFO",
+        "Checking the request args...",
+        dict(job_phase="check_request_args")
+    )
+    if not request_json:
+        return gcp_log(
+            "ERROR",
+            "The body of the request is missing. Please enter a body with valid values for: {FUNCTION_ARGS}.",
+            dict(job_phase=job_phase,
+                 job_phase_detail="request_body")
+        )
+
+    # Check optional args
+    for arg in FUNCTION_ARGS['optional']:
+        if arg in request_json and request_json[arg] is not None:
+            inputs_dict[arg] = request_json[arg]
+        else:
+            inputs_dict[arg] = DEFAULTS_ARG_VALUES[arg]
+            gcp_log(
+                "WARNING",
+                f"The '{arg}' wasn't provided or the value is None. Using default value = '{DEFAULTS_ARG_VALUES[arg]}' instead.",
+                dict(job_phase=job_phase,
+                     job_phase_detail="optional_args",
+                     arg_details={
+                         "name": arg,
+                         "value": DEFAULTS_ARG_VALUES[arg]
+                     })
+            )
+
+    # Check required args
+    for arg in FUNCTION_ARGS['required']:
+        print("-----arg", arg)
+        if arg in request_json and request_json[arg] is not None:
+            inputs_dict[arg] = request_json[arg]
+            if arg == 'client_name':
+                GLOBAL_LOG_FIELDS['client_name'] = request_json['client_name']
+        else:
+            return gcp_log(
+                "ERROR",
+                f"The '{arg}' wasn't provided or the value is None. Please enter a value for '{arg}'.",
+                dict(job_phase=job_phase,
+                     job_phase_detail="required_args",
+                     arg_details={
+                         "name": arg,
+                         "value": request_json.get(arg),
+
+                     })
+            )
+
+        # if arg == 'client_namme':
+        #     GLOBAL_LOG_FIELDS['client_name'] = inputs_dict['client_name']
+        #     print("-----GLOBAL_LOG_FIELDS", GLOBAL_LOG_FIELDS)
+
+    GLOBAL_LOG_FIELDS['input_params'] = inputs_dict
+    gcp_log(
+        "NOTICE",
+        f"All request arguments checked. Parameters: {inputs_dict}",
+        {"parameters_all": inputs_dict}
+    )
+
+    # print("-----inputs_dict", inputs_dict)
+    return (inputs_dict, 200)
+
+
+def gcp_log(severity, message, additional_log_fields=None):
+    """
+    Logs a message into Google Cloud Functions logs.
+
+    Args:
+        severity (str): The severity of the log message. Supported values are:
+            "DEBUG", "INFO", "NOTICE", "WARNING", "ERROR", "CRITICAL", "ALERT",
+            "EMERGENCY".
+        message (str): The message to be logged.
+        **kwargs: Additional keyword arguments to be added to the log entry.
+    """
+    if additional_log_fields is None:
+        additional_log_fields = {}
+
+    # Add client_name to additional_log_fields if it is present in inputs_dict
+    # if 'client_name' in inputs_dict:
+    #     additional_log_fields['client_name'] = inputs_dict['client_name']
+
+    additional_log_fields = {**GLOBAL_LOG_FIELDS, **additional_log_fields}
+
+    log_entry = dict(
+        severity=severity.upper(),
+        message=message,
+        component="Keboola Orchestration-v2 Trigger",
+        **additional_log_fields
+    )
+
+    print(json.dumps(log_entry))
+
+    if severity.upper() == "ERROR":
+        # Raise a RuntimeError if the severity is ERROR
+        # This will terminate the function and the error will be visible in the
+        # Cloud Functions logs
+
+        # raise RuntimeError(message)
+        # return [json.dumps({"error": message}), 400]
+        return ({"error": message, "details": additional_log_fields}, 400)
+
+    return ({"message": message, "details": additional_log_fields}, 200)
+
+
+def create_job(keboola_endpoint_url, keboola_storage_api_token, keboola_orchestration_trigger_run_id):
+    """
+    Creates a new job in the Keboola Orchestrator API.
+
+    Args:
+        keboola_endpoint_url (str): The URL of the Keboola Orchestrator API.
+        keboola_storage_api_token (str): The Storage API token to use for authentication.
+        keboola_orchestration_trigger_run_id (str): The ID of the orchestration trigger run.
+
+    Returns:
+        str: The ID of the created job.
+
+    Raises:
+        Exception: If the job creation fails.
+    """
+
+    # Log information that job will be created
+    gcp_log(
+        "INFO",
+        f"Creating a job for orchestration trigger with id: {keboola_orchestration_trigger_run_id}",
+        {
+            "job_phase": "create_job",
+            "parameters_all": inputs_dict
+        }
+    )
+    try:
+        result = requests.post(
+            url=f"{keboola_endpoint_url}/jobs",
+            headers={
+                "X-StorageApi-Token": keboola_storage_api_token,
+                "X-KBC-RunId": keboola_orchestration_trigger_run_id,
+            },
+            json={
+                "component": KEBOOLA_COMPONENT_NAME,
+                "config": keboola_orchestration_trigger_run_id,
+                "mode": KEBOOLA_MODE,
+                "configRowIds": [],
+            }
+        )
+        result_json = result.json()
+        if 'error' in result_json:
+            return gcp_log(
+                "ERROR",
+                f"The function failed to start a job for orchestration trigger with id: {keboola_orchestration_trigger_run_id}. The response from API: {result_json['error']}",
+                dict(job_phase="create_job",
+                     job_phase_detail="error_response_from_api",
+                     error_message=result_json['error'])
+
+            )
+        job_id = result_json.get("id")
+
+        return (job_id, 200)
+    except Exception as e:
+        # gcp_log("ERROR", f"The function failed to start a job for a orchestration trigger with id: {keboola_orchestration_trigger_run_id}", **dict(
+        #     GLOBAL_LOG_FIELDS, job_phase="create_job"))
+        return gcp_log(
+            "ERROR",
+            f"Exception in create_job for orchestration trigger with id: {keboola_orchestration_trigger_run_id}. Exception: {e}",
+            dict(
+                job_phase="create_job",
+                job_phase_detail="error_creating_job",
+                error_message=f"{e}"
+            )
+        )
+
+
+def check_job_status(keboola_endpoint_url, keboola_storage_api_token, keboola_job_id, keboola_job_max_runtime_seconds):
+    start_time = time.time()
+    job_status = None
+    job_phase = "check_job_status"
+
+    gcp_log(
+        "INFO",
+        f"Checking the status of the job with id: {keboola_job_id}",
+        dict(job_phase="check_job_status")
+    )
+
+    try:
+        while time.time() - start_time < keboola_job_max_runtime_seconds:
+            elapsed_time = round(time.time() - start_time, 2)
+
+            result = requests.get(
+                url=f"{keboola_endpoint_url}/jobs/{keboola_job_id}",
+                headers={
+                    "X-StorageApi-Token": keboola_storage_api_token,
+                }
+            )
+            result_json = result.json()
+            job_status = result_json.get("status")
+
+            # Print the time elapsed every 30 seconds
+            if int(elapsed_time) % 30 == 0 and int(elapsed_time) != 0:
+                gcp_log(
+                    "INFO",
+                    f"Job ({keboola_job_id}) status check... Time elapsed: {elapsed_time} seconds.",
+                    dict(job_phase=job_phase, details=result_json)
+                )
+
+            if 'error' in result_json:
+                kill_job(keboola_endpoint_url,
+                         keboola_storage_api_token, keboola_job_id)
+
+                return gcp_log(
+                    "ERROR",
+                    f"The function failed to check the status of the job with id: {keboola_job_id}. The response from API: {result_json['error']}",
+                    dict(job_phase=job_phase,
+                         job_phase_detail="error_response_from_api",
+                         error_message=result_json['error'])
+                )
+            if job_status in ["failed", "success"]:
+                if job_status == "failed":
+                    severity = "ERROR"
+                else:
+                    severity = "INFO"
+
+                return gcp_log(
+                    severity,
+                    f"The job with id: {keboola_job_id} finished with status: {job_status}.",
+                    dict(job_phase=job_phase,
+                         job_phase_detail="job_finished", job_status=job_status)
+                )
+                break
+
+            time.sleep(10)
+
+        # return True
+        if job_status not in ['failed', 'success']:
+            kill_job(keboola_endpoint_url,
+                     keboola_storage_api_token, keboola_job_id)
+            return gcp_log(
+                "ERROR",
+                f"Job with ID: {keboola_job_id} was terminated due to timeout = {keboola_job_max_runtime_seconds} seconds.",
+                dict(job_phase=job_phase,
+                     job_phase_detail="job_timeout", job_status=job_status)
+            )
+
+            # return ["Killed", job_status]
+        else:
+            return ("Executed", 200)
+    except Exception as e:
+
+        kill_job(keboola_endpoint_url,
+                 keboola_storage_api_token, keboola_job_id)
+
+        return gcp_log(
+            "ERROR",
+            f"The function failed to check the status of the job with id: {keboola_job_id}.",
+            dict(job_phase=job_phase,
+                 job_phase_detail="error_checking_job", error_message=f"{e}")
+        )
+
+    # kill_job_response = kill_job(
+    #     keboola_endpoint_url, keboola_storage_api_token, keboola_job_id)
+
+
+def kill_job(keboola_endpoint_url, keboola_storage_api_token, keboola_job_id):
+    gcp_log(
+        "INFO",
+        f"Killing job with ID: {keboola_job_id}.",
+        dict(job_phase="kill_job")
+    )
+    try:
+        result = requests.post(
+            url=f"{keboola_endpoint_url}/jobs/{keboola_job_id}/kill",
+            headers={
+                "X-StorageApi-Token": keboola_storage_api_token,
+            }
+        )
+
+        if result.status_code == 200:
+            gcp_log(
+                "INFO",
+                f"Job with ID: {keboola_job_id} killed successfully.",
+                dict(job_phase="kill_job")
+            )
+            # gcp_log(
+            #     "ERROR",
+            #     f"Job with ID: {keboola_job_id} was terminated due to timeout = {keboola_job_max_runtime_seconds}.",
+            # )
+        else:
+            return gcp_log(
+                "ERROR",
+                f"Failed to kill job with ID: {keboola_job_id}. Status Code: {result.status_code}",
+                dict(job_phase="kill_job")
+            )
+            # print(
+            #     f"Failed to kill job {keboola_job_id}. Status Code: {result.status_code}")
+
+        return ("Success", 200)
+    except Exception as e:
+        return gcp_log(
+            "ERROR",
+            f"Failed to kill job with ID: {keboola_job_id}.",
+            dict(job_phase="kill_job", error_message=f"{e}")
+        )
